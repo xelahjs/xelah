b102ba5057ccceea9700491828d032f5016f1028
klappy <christopher@klapp.name>
2022-07-12 12:47:20 -0400

    update styleguidist package name

--- packages/@ster-core/styleguide.config.js
+++ packages/@ster-core/styleguide.config.js
@@ -1,22 +1,21 @@
 const path = require('path');
-const upperFirst = require('lodash/upperFirst');
-const camelCase = require('lodash/camelCase');
 
 const {
-  name, version, repository,
+  name: packageName, version, repository,
 } = require('./package.json');
 
 module.exports = {
   usageMode: 'expand',
   exampleMode: 'expand',
-  moduleAliases: { 'ster-core': path.resolve(__dirname, 'src') },
   getComponentPathLine: componentPath => {
     const name = path.basename(componentPath, '.js');
-    return `import { ${name} } from 'ster-core';`;
+    return `import { ${name} } from '${packageName}';`;
   },
-  title: `${upperFirst(camelCase(name))} v${version}`,
+  title: `${name} v${version}`,
   ribbon: {
     url: repository.url,
     text: 'View on GitHub',
   },
 };
+
+module.exports.moduleAliases[packageName] = path.resolve(__dirname, 'src');
