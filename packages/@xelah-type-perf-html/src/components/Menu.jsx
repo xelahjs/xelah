8600d3dfe092d4362928c225bbea0bcbb71c3326
David Stone <david@nnucomputerwhiz.com>
2023-01-05 10:34:33 -0700

    Remove buttons which don't work yet

--- packages/@xelah-type-perf-html/src/components/Menu.jsx
+++ packages/@xelah-type-perf-html/src/components/Menu.jsx
@@ -23,14 +23,12 @@ export default function Menu() {
           <FormatBlock label="\q1 poetic line level 1" subtype="q1" sequence="main" />
           <FormatBlock label="\q2 poetic line level 2" subtype="q2" sequence="main" />
           <FormatBlock label="\q3 poetic line level 3" subtype="q3" sequence="main" />
-          <FormatBlock label="\imt introduction major title" subtype="imt" sequence="introduction" />
-          <FormatBlock label="\is introduction section heading" subtype="is" sequence="introduction" />
           <FormatBlock label="\ip introduction paragraph" subtype="ip" sequence="introduction" />
           <FormatBlock label="\im introduction paragraph no ident" subtype="im" sequence="introduction" />
           <FormatBlock label="\ipi introduction paragraph with ident" subtype="ipi" sequence="introduction" />
           <FormatBlock label="\iot introduction outline title" subtype="iot" sequence="introduction" />
           <FormatBlock label="\io introduction outline level 1" subtype="io" sequence="introduction" />
-          <FormatBlock label="\io2 introduction paragraph level 2" subtype="io2" sequence="introduction" />
+          <FormatBlock label="\io2 introduction outline level 2" subtype="io2" sequence="introduction" />
         </>
       ) : null}
     </div>
