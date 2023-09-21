03c3cb78f95260d2601c31f3e138b2c9db100ecb
Noah Harvey <noah.harvey247@gmail.com>
2023-09-21 15:00:14 -0400

    adds jsdoc for segmenter

--- packages/@xelah-core/src/helpers/segmenter.js
+++ packages/@xelah-core/src/helpers/segmenter.js
@@ -1,3 +1,13 @@
+/**
+@todo Is this used directly anywhere? A quick search on github shows no
+results (only Editable* from @xelah/core are used both across other xelah
+modules and some friendsofagape modules). 
+
+Further: this function is identical to String.<prototype>.match, I'm curious
+to existance. 
+
+See [MDN Documentation]{@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec#description}
+*/
 export const segmenter = ({ content, regex }) => {
   let segments = [content];
   segments = [];
